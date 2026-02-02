96d4ce0e9cf99234a9b3048c333c5a60cb83180f
Jaagup Kippar <jaagup@tlu.ee>
2026-02-02 13:15:07 +0000

    T-testi algus

---
+++ 02_t_test/t_test1.py
@@ -0,0 +1,8 @@
+from scipy.stats import ttest_ind
+print(ttest_ind([3, 5, 4], [12, 16, 14]))
+print(ttest_ind([3, 5, 4, 6, 5, 4], [12, 16, 14, 16, 15, 14]))
+print(ttest_ind([13, 15, 14], [12, 16, 14]))
+print(ttest_ind([13, 15, 14, 18], [12, 16, 14, 11]))
+
+
+# pip install scipy
