530b30bba0d5e83d7b479c2738c5aa788109be07
raheltoom3 <toomerahel@gmail.com>
2026-02-08 15:53:50 +0000

    updated

--- 02_t_test/kodutöö_09.02.py
+++ 02_t_test/kodutöö_09.02.py
@@ -1,6 +1,6 @@
 from scipy.stats import ttest_ind
 
-#Kas meeste ja naiste keskmine pikkus erineb üksteisest rohkem kui see erinevus võiks tekkida juhuslikult?
+#Kas meeste ja naiste pikkuste vahe on päriselt olemas või võiks see vahe tekkida lihtsalt juhuslikult?
 
 m_pikkused = [172, 175, 180, 178, 182, 176]
 n_pikkused = [163, 162, 165, 158, 164, 167]
@@ -8,6 +8,32 @@ n_pikkused = [163, 162, 165, 158, 164, 167]
 print(ttest_ind(m_pikkused, n_pikkused))
 
 #Kasutasin sõltumatute valimite t-testi, et hinnata, kas kahe rühma keskmiste vahe on statistiliselt oluline. 
-#T-test näitab, kas meeste ja naiste pikkuste erinevus on suurem, kui seda võiks eeldada juhusliku varieeruvuse tõttu.
 
-#TtestResult=7.256494774883425
+#TtestResult(statistic=np.float64(7.256494774883425), pvalue=np.float64(2.7375353557920887e-05), df=np.float64(10.0))
+
+#7.26 viitab sellele, kui suur vahe on võrreldes juhuslikkusega - pigem suur
+
+#2.7 kui meeste ja naiste keskmine pikkus oleks tegelikult sama, siis kui suure tõenäosusega need arvud olid juhuslikud - väga ebatõenäoline
+
+#Meeste ja naiste keskmine pikkus erineb statistiliselt olulisel määral.
+#See erinevus ei ole juhuslik, vaid väga suure tõenäosusega päriselt olemas.
+
+
+#Aritmeetline keskmine
+
+def keskmine(m):
+    return sum(m) / len(m)
+
+print(keskmine(m_pikkused), keskmine(n_pikkused))
+
+#Statsmodels
+
+import statsmodels.stats.api as sms
+
+cm = sms.CompareMeans(
+    sms.DescrStatsW(m_pikkused),
+    sms.DescrStatsW(n_pikkused)
+)
+
+print(cm.tconfint_diff(usevar='unequal'))
+
