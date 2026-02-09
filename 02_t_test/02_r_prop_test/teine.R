398397e68b4eb728e729b207db7247caa5957779
raheltoom3 <toomerahel@gmail.com>
2026-02-09 13:45:02 +0000

    updated

---
+++ 02_t_test/02_r_prop_test/teine.R
@@ -0,0 +1,7 @@
+print(prop.test(2, 12)) 
+#12 õpilast aga 2 poissi.
+#mida sellest järeldada võin:
+#kui neid mõnel teisel kursusel 12 õpilase hulgast on 1-5, siis pole põhjust eripära otsuda
+
+
+#kõikumise vahemik: 95 percent confidence interval: 0.02940911 0.49118581
